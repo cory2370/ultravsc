58acccd992454f3c0d57dbf6d55d232ba5db98b4
Ngo Quang Tung <tung061741@stu.vinschool.edu.vn>
2025-11-23 18:38:41 +0700

    Change base URL based on NODE_ENV

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: '.',
-  base: '/ultravsc/',
+  base: process.env.NODE_ENV === 'production' ? '/ultravsc/' : '/',
   build: {
     outDir: 'dist',
   },
   publicDir: 'public',
 })
-
